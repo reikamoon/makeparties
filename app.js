c0baa91a4994151094a80f4d78cc252577d8fd1d
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-04 18:56:39 -0700

    proper separation of concerns

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-// INITIALIZE BODY-PARSER AND ADD IT TO APP
-const bodyParser = require('body-parser');
-const models = require('./db/models');
-
-require('./controllers/events')(app, models);
-
 // Initialize express
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+const models = require('./db/models');
+
 
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'))
 
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./controllers/events')(app, models);
+
 
 const handlebars = require('handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
@@ -27,23 +29,7 @@ app.engine('handlebars', hbs.engine); 
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
-// app.use(methodOverride('_method'));
 
-//
-// // require handlebars
-// var exphbs = require('express-handlebars');
-//
-// // Use "main" as our default layout
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// // Use handlebars to render
-// app.set('view engine', 'handlebars');
-
-// app.js
-
-// // Render the "home" layout for the main page and send the following msg
-// app.get('/', (req, res) => {
-//   res.render('home', { msg: 'Handlebars are Cool!' });
-// })
 
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
@@ -52,65 +38,6 @@ var events = [
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// NEW
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-})
-
-// CREATE
-app.post('/events', (req, res) => {
-  models.Event.create(req.body).then(event => {
-    // Redirect to events/:id
-    res.redirect(`/events/${event.id}`)
-
-  }).catch((err) => {
-    console.log(err)
-  });
-})
-
-// SHOW
-app.get('/events/:id', (req, res) => {
-  // Search for the event by its id that was passed in via req.params
-  models.Event.findByPk(req.params.id).then((event) => {
-    // If the id is for a valid event, show it
-    res.render('events-show', { event: event })
-  }).catch((err) => {
-    // if they id was for an event not in our db, log an error
-    console.log(err.message);
-  })
-})
-
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-  models.Event.findByPk(req.params.id).then((event) => {
-    res.render('events-edit', { event: event });
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-// DELETE
-app.delete('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.destroy();
-    res.redirect(`/`);
-  }).catch((err) => {
-    console.log(err);
-  });
-})
 
 // Tell our app to send the "hello world" message to our home page
 // app.get('/', (req, res) => {
--- controllers/events.js
+++ controllers/events.js
@@ -8,4 +8,64 @@ module.exports = function (app, models) {
             res.render('events-index', { events: events });
         })
     })
+
+    // NEW
+    app.get('/events/new', (req, res) => {
+      res.render('events-new', {});
+    })
+
+    // CREATE
+    app.post('/events', (req, res) => {
+      models.Event.create(req.body).then(event => {
+        // Redirect to events/:id
+        res.redirect(`/events/${event.id}`)
+
+      }).catch((err) => {
+        console.log(err)
+      });
+    })
+
+    // SHOW
+    app.get('/events/:id', (req, res) => {
+      // Search for the event by its id that was passed in via req.params
+      models.Event.findByPk(req.params.id).then((event) => {
+        // If the id is for a valid event, show it
+        res.render('events-show', { event: event })
+      }).catch((err) => {
+        // if they id was for an event not in our db, log an error
+        console.log(err.message);
+      })
+    })
+
+    // EDIT
+    app.get('/events/:id/edit', (req, res) => {
+      models.Event.findByPk(req.params.id).then((event) => {
+        res.render('events-edit', { event: event });
+      }).catch((err) => {
+        console.log(err.message);
+      })
+    });
+
+    // UPDATE
+    app.put('/events/:id', (req, res) => {
+      models.Event.findByPk(req.params.id).then(event => {
+        event.update(req.body).then(event => {
+          res.redirect(`/events/${req.params.id}`);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    // DELETE
+    app.delete('/events/:id', (req, res) => {
+      models.Event.findByPk(req.params.id).then(event => {
+        event.destroy();
+        res.redirect(`/`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    })
 }
