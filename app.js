5ef2f44ced60fc6063d41e282f6206c248e239dc
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-04 18:33:16 -0700

    users can now see single events

--- app.js
+++ app.js
@@ -68,12 +68,28 @@ app.get('/events/new', (req, res) => {
 // })
 
 // CREATE
+// CREATE
 app.post('/events', (req, res) => {
   models.Event.create(req.body).then(event => {
-    res.redirect(`/`);
+    // Redirect to events/:id
+    res.redirect(`/events/${event.id}`)
+
   }).catch((err) => {
     console.log(err)
   });
+})
+
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+  // Search for the event by its id that was passed in via req.params
+  models.Event.findByPk(req.params.id).then((event) => {
+    // If the id is for a valid event, show it
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    // if they id was for an event not in our db, log an error
+    console.log(err.message);
+  })
 })
 
 // Tell our app to send the "hello world" message to our home page
